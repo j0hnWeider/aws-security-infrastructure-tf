fb54ac70aa033ae01779032125224a11a8cabe6c
John Weider <88955656+j0hnWeider@users.noreply.github.com>
2026-05-20 10:14:37 -0300

    Add comment for final architectural security layer
    Added a comment indicating the final architectural security layer.

--- main.tf
+++ main.tf
@@ -82,3 +82,5 @@ resource "aws_route_table_association" "public_assoc" {
   subnet_id      = aws_subnet.public_subnet_1a.id
   route_table_id = aws_route_table.public_rt.id
 }
+
+# Final architectural security layer applied.
